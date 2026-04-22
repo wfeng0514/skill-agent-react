66e3a46d838a55fa515b3e07bd32cb40a9fa0c19
wfeng <3022568959@qq.com>
2026-04-22 15:31:22 +0800

    refactor: 去除构建文件

--- .mastra/output/tools.mjs
+++
@@ -1,7 +0,0 @@
-import * as tool0 from './tools/1b781677-0d62-4cf7-8807-bf2ddd99694d.mjs';
-import * as tool1 from './tools/1643ac3c-037d-466a-9e73-189811bf996c.mjs';
-import * as tool2 from './tools/89a54ae3-fef3-4e48-b647-3fadaa1050f8.mjs';
-import * as tool3 from './tools/840b0d01-f722-41df-91cd-b526cb68ecae.mjs';
-import * as tool4 from './tools/ab124ee5-5126-462f-bf38-d937b50f3c00.mjs';
-
-                export const tools = [tool0, tool1, tool2, tool3, tool4]
